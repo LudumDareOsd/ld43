e24701c969e09d88d5bc3f97a224bd47b1ed9129
Roland Egedi <roland.egedi@invativa.se>
2018-12-01 18:00:05 +0100

    Title screen

--- src/scenes/LoadScene.ts
+++ src/scenes/LoadScene.ts
@@ -6,9 +6,27 @@ class LoadScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image('background_title', 'assets/gfx/title_screen.png');
+    this.load.image('startguld', 'assets/gfx/startguld.png');
+
     this.load.image('tilemap01', '/assets/gfx/tilemap01.png');
     this.load.image('background-tiles', '/assets/gfx/background-tiles.png');
     this.load.tilemapTiledJSON('map', '/assets/maps/test.json');
+
+    this.load.audio('titleaudio', 'assets/sfx/ambient_deep_bass.mp3', null);
+    this.load.audio('knife_hit', 'assets/sfx/knife_hit.mp3', null);
+    this.load.audio('player_death', 'assets/sfx/player_death.mp3', null);
+    this.load.audio('player_fire_knife', 'assets/sfx/player_fire_knife.mp3', null);
+    this.load.audio('player_hurt', 'assets/sfx/player_hurt.mp3', null);
+    this.load.audio('player_jump', 'assets/sfx/player_jump.mp3', null);
+    this.load.audio('player_pickup', 'assets/sfx/player_pickup.mp3', null);
+    this.load.audio('player_pickup_soul', 'assets/sfx/player_take_soul.mp3', null);
+    this.load.audio('popehat_death', 'assets/sfx/popehat_death.mp3', null);
+    this.load.audio('popehat_flying', 'assets/sfx/popehat_flying.mp3', null);
+    this.load.audio('portal_activated', 'assets/sfx/portal_activated.mp3', null);
+    this.load.audio('portal_use', 'assets/sfx/portal_use.mp3', null);
+    this.load.audio('priest_death', 'assets/sfx/priest_death.mp3', null);
+    this.load.audio('priest_fire', 'assets/sfx/priest_fire.mp3', null);
   }
 
 	update(time: number, delta:number) {
--- src/scenes/StartScene.ts
+++ src/scenes/StartScene.ts
@@ -1,18 +1,44 @@
 class StartScene extends Phaser.Scene {
+
+	private music : any;
+
 	constructor() {
-    super({
-			key: 'StartScene'
-		});
+		super({
+				key: 'StartScene'
+			});
 	}
 
 	preload() {
+		this.music = this.sound.add('titleaudio', { loop: true });
 	}
 
 	create() {
+		this.add.image(0, 0, 'background_title').setOrigin(0, 0);
+		let start = this.add.sprite(448, 583, 'startguld').setOrigin(0, 0); start.alpha = 0;
+		this.music.play('', 0, 1, true);
+
+		this.add.zone(500, 280, 800, 100).setName('StartGame').setInteractive();
+
+		this.input.on('gameobjectdown', (pointer, gameObject) => {
+            if(gameObject.name == 'StartGame') {
+                document.getElementsByTagName('canvas')[0].style.cursor = "default";
+                this.music.stop();
+                this.scene.start('GameScene');
+            }
+		});
+		
+		this.input.on('pointerover', (event) => {
+            document.getElementsByTagName('canvas')[0].style.cursor = "crosshair";
+        });
+
+        this.input.on('pointerout', (event) => {
+            document.getElementsByTagName('canvas')[0].style.cursor = "default";
+        });
 	}
 
 	update(time: number, delta:number) {
-    this.scene.start('GameScene');
+		this.music.stop();
+    	this.scene.start('GameScene');
 	}
 }
 
