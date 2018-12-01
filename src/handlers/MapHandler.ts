4be1a11a04ee4aa35d921fe1a856af7f193aa2b8
pontus-persson <eid.khorm@gmail.com>
2018-12-01 16:16:11 +0100

    BG layer etc

--- src/handlers/MapHandler.ts
+++ src/handlers/MapHandler.ts
@@ -4,6 +4,7 @@ class MapHandler {
   private map;
   private tileLayer;
   private deadlyLayer;
+  private backgroundLayer;
 
   constructor({scene: scene}) {
     this.sceneRef = scene;
@@ -12,16 +13,34 @@ class MapHandler {
   create() {
     this.map = this.sceneRef.make.tilemap({key: 'map'});
     const tiles = this.map.addTilesetImage('tilemap01', 'tilemap01');
-    this.tileLayer = this.map.createStaticLayer(0, tiles, 0, 0).setScale(2);
+    const bgtiles = this.map.addTilesetImage('background-tiles', 'background-tiles');
+    this.tileLayer = this.map.createStaticLayer('Tiles', tiles, 0, 0).setScale(2);
+    this.deadlyLayer = this.map.createStaticLayer('Deadly', tiles, 0, 0).setScale(2);
+    this.backgroundLayer = this.map.createStaticLayer('Background', bgtiles, 0, 0).setScale(2);
     // const belowLayer = this.map.createStaticLayer('Below Player', tileset, 0, 0);
+    // this.deadlyGroup = this.sceneRef.physics.add.group();
 
     this.tileLayer.setCollisionByExclusion([-1]);
+    this.deadlyLayer.setCollisionByExclusion([-1]);
+
+    // map.setCollisionBetween(1, 999, true, 'collisionLayer');
   }
 
   init() {
     this.sceneRef.physics.add.collider(this.tileLayer, this.sceneRef.player, null, null, null);
+    this.sceneRef.physics.add.collider(this.deadlyLayer, this.sceneRef.player, this.playerDeadlyCollide, null, null);
     this.sceneRef.physics.world.bounds.width = this.tileLayer.width;
     this.sceneRef.physics.world.bounds.height = this.tileLayer.height;
+  }
+
+  playerDeadlyCollide(player, tile) {
+    // console.log(tile, player);
+    console.log('kill');
+    player.body.x = 100;
+    player.body.y = 100;
+    player.body.setVelocityX(0);
+    player.body.setVelocityY(0);
+    // player.anims.play('hurt');
   }
 
 }
